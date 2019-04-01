69dce29cd2b984f6448954737850ad7a01e9def0
Jose Tobar <josectobar@gmail.com>
2019-04-01 13:16:19 -0600

    connected component to auth0 logging, getting token

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
-import Home from './Home/Home'
-import Profile from './Profile/Profile'
-import Nav from './Nav/Nav'
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Home from "./Home/Home";
+import Profile from "./Profile/Profile";
+import Nav from "./Nav/Nav";
+import "./App.css";
+import Auth from "./Auth/Auth";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
   render() {
     return (
       <>
-       <Nav/>
-       <Route path='/' exact component={Home} />
-       <Route path='/profile' exact component={Profile} />
+        <Nav />
+        <Route
+          path="/"
+          exact
+          render={props => <Home auth={this.auth} {...props} />}
+        />
+        <Route path="/profile" exact component={Profile} />
       </>
     );
   }
--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,25 +1,17 @@
-import React, { Component } from 'react';
-import auth0 from 'auth0-js'
+import auth0 from "auth0-js";
 
-class Auth extends Component {
-    constructor(history){
-        this.history = history
-        this.auth0 = new auth0.WebAuth({
-            domain: process.env.REACT_APP_AUTH0_DOMAIN,
-            clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
-            redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
-            responseType: "token id_token",
-            scope: "openid profile email"
-        })
-    }
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+export default class Auth {
+  constructor(history) {
+    this.history = history;
+    this.auth0 = new auth0.WebAuth({
+      domain: process.env.REACT_APP_AUTH0_DOMAIN,
+      clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+      redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
+      responseType: "token id_token",
+      scope: "openid profile email"
+    });
+  }
+  login = () => {
+    this.auth0.authorize();
+  };
 }
-import auth0 from 'auth0-js'
-
-export default Auth;
